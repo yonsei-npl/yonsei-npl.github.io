65acff6f948a98d62febcde80cc1d56bcbbed67a
yonsei-npl <yonsei.physics.npl@gmail.com>
2026-09-14 16:53:13 +0000

    Deploying to gh-pages from @ yonsei-npl/yonsei-npl.github.io@a0e0a887aea8115ea5d027a23632f6e63ff494ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "Publications",
-          description: "Selected scholarly works from the laboratory.",
+          description: "Selected Publications from the Laboratory",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
